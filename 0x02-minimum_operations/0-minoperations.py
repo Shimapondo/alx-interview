c61acfc4f5811aa838b7b86cbc6a113e6f757c68
Shimapondo <williamnamabende@gmail.com>
2024-04-26 16:00:59 +0300

    My new code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""module contains a function min_operations that calculates
-    the least number of operations to take to reach a given number"""
-import math
+""" Module for 0-minoperations"""
 
 
 def minOperations(n):
-    """function gets one argument, the number to find the least number
-        steps to reach
-        i.e given 9 we might have:
-            1, 2, 3, 6, 9:
-        copy paste, paste, copy paste, paste
-        giving 6 minimum operations to copy paste a number"""
-    min_operations = 0  # number of operations to take
-    current_value = 0
-    step = 1  # step in incrementing the current_value
-    if (math.isinf(n) or n <= 0 or isinstance(n, float)):
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-    while (current_value < n):
-        # check if current value divides n and is not 0 or 1
-        if not (current_value in [0, 1]) and (n % current_value == 0):
-            # change the step to the number, copying in our case
-            step = current_value
-            min_operations += 1  # add one as this is a copy operation
-        current_value += step  # our paste operation
-        min_operations += 1  # adding one as the above was a paste opertaion
-    return min_operations
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
